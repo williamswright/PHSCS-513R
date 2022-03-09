5d59b13c40bcc6be80e8f49647a953c4cca8062f
William Wright <williamsanfordwright@gmail.com>
2022-03-08 18:34:42 -0700

    just to make sure eveything is up to date

--- NumericalDerivatives/FiniteDifferences (Problem 1).jl
+++ NumericalDerivatives/FiniteDifferences (Problem 1).jl
@@ -5,14 +5,36 @@ using Plots
 a = -pi
 b = pi
 N = 100
-h = (a - b)/N
+h = (b - a)/N
 
 # Make base array
 x = collect(a:h:b)
 
 # Compute the derivative of sin(x)
 y = sin.(x)
-yp = (sin.(x[3:end]) - sin.(x[1:end-3]))/(2*h)
+yp = (sin.(x[3:end]) - sin.(x[1:end-2]))/(2*h)
 
 # Plot error in derivative
-println(y)
+plot(x[1:end-2],yp - cos.(x[1:end-2]),label = "")
+title!("Error in Unoptimized Derivative")
+xlabel!("x") 
+ylabel!("y'(x) - cos(x)")
+
+# Set parameters for first part
+a = -pi
+b = pi
+N = 100
+h = (b - a)/N
+
+# Make base array
+x = collect(a:h:b)
+
+# Compute the derivative of sin(x)
+y = sin.(x)
+yp = (sin.(x[3:end]) - sin.(x[1:end-2]))/(2*h)
+
+# Plot error in derivative
+plot(x[1:end-2],yp - cos.(x[1:end-2]),label = "")
+title!("Error in Optimized derivative")
+xlabel!("x") 
+ylabel!("y'(x) - cos(x)")
